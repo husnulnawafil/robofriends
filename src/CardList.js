1d99604c9a7e20301ec0dded013f2b5731cf8bdc
husnulnawafil <husnulnawafil27@gmail.com>
2022-11-06 11:36:33 +0700

    add search box component

--- src/CardList.js
+++ src/CardList.js
@@ -5,15 +5,17 @@ import Card from './Card';
 const CardList = ({robots})=>{
     return (
         <>
-            {
-                robots.map((data, index)=>{
-                    return <Card 
-                    key={robots[index].id}
-                    id={robots[index].id} 
-                    name={robots[index].name}
-                    email={robots[index].email}/>
-                })
-            }
+            <div className="tc">
+                {
+                    robots.map((data, index)=>{
+                        return <Card 
+                        key={robots[index].id}
+                        id={robots[index].id} 
+                        name={robots[index].name}
+                        email={robots[index].email}/>
+                    })
+                }
+            </div>
         </>
     );
 }
